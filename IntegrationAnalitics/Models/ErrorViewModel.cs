8960c9bf7435638d3d5bdf72b80f9ef1167b0ff9
Константин Ларионов <kostya12277@yandex.ru>
2022-12-21 18:39:36 +0500

    ~~

---
+++ IntegrationAnalitics/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IntegrationAnalitics.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
